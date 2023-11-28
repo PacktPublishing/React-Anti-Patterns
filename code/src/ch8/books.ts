eadcfb4ff22ad07cce48aba8cd0529ce5964158a
Juntao Qiu <juntao.qiu@gmail.com>
2023-11-28 17:00:38 +1100

    added a list data example

---
+++ code/src/ch8/books.ts
@@ -0,0 +1,26 @@
+export const books = [
+  {
+    id: "1",
+    name: "Refactoring: Improving the Design of Existing Code",
+    description:
+      "Martin Fowler's cornerstone work on refactoring is a repository of knowledge on how to enhance the structure of your code while preserving its functionality and bug-free nature. It's a must-read for anyone aspiring to hone their refactoring skills.",
+  },
+  {
+    id: "2",
+    name: "Clean Code: A Handbook of Agile Software Craftsmanship",
+    description:
+      "Robert Martin's Clean Code is a hallmark in the software development world. It delves into various practices and principles of penning clean, maintainable code, which is crucial for long-term success in complex projects.",
+  },
+  {
+    id: "3",
+    name: "Patterns of Enterprise Application Architecture",
+    description:
+      "Expand your architectural vistas with Martin Fowler's Patterns of Enterprise Application Architecture. This book dissects various patterns crucial for designing robust and scalable enterprise applications. It's a significant read to grasp the bigger picture of application architecture, extending beyond the frontend realm.",
+  },
+  {
+    id: "4",
+    name: "Test-Driven Development with React and TypeScript",
+    description:
+      "Immerse yourself in the world of test-driven development (TDD) with a lens on React and TypeScript. This book navigates you through the principles of TDD and how it can markedly improve the quality, maintainability, and robustness of your code.",
+  },
+];
